253eb846c8b07663f71d0155e89e8e2c55b07b3d
jamescardona11 <jjcardonoa@gmail.com>
2020-05-25 13:36:28 -0500

    Create bloc from scratch

---
+++ lib/counter/counterfromscratch/counter_from_scratch.dart
@@ -0,0 +1,36 @@
+import 'dart:async';
+
+import 'package:blocappflutter/counter/counter_event.dart';
+
+class CounterBlocScratch {
+  int _counter = 0;
+
+  final _counterStateController = StreamController<int>();
+  StreamSink<int> get _inCounter => _counterStateController.sink;
+
+  Stream<int> get counter => _counterStateController.stream;
+
+  final _counterEventController = StreamController<CounterEvent>();
+  Sink<CounterEvent> get counterEventSink => _counterEventController.sink;
+
+  CounterBlocScratch() {
+    _counterEventController.stream.listen(mapEventToState);
+  }
+
+  void mapEventToState(CounterEvent event) {
+    switch (event) {
+      case CounterEvent.increment:
+        _counter++;
+        break;
+
+      case CounterEvent.decrement:
+        _counter--;
+        break;
+    }
+  }
+
+  void dispose() {
+    _counterStateController.close();
+    _counterEventController.close();
+  }
+}
