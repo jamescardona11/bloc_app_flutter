94bf2bc1be660f77b915d298b7c87c29dc606a6a
jamescardona11 <jjcardonoa@gmail.com>
2020-05-25 14:16:42 -0500

    Create all states the abstract TimeState

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:blocappflutter/counter/counterfromscratch/from_scratch_page.dart';
 import 'package:blocappflutter/counter/counterlibrary/from_library_page.dart';
+import 'package:blocappflutter/timer/timer_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Bloc Demo',
-      home: FromLibraryPage(),
+      home: TimerPage(),
     );
   }
 }
---
+++ lib/timer/timer_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class TimerPage extends StatelessWidget {
+  static final String id = 'timer_page_route_id';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('TImer Page'),
+      ),
+    );
+  }
+}
